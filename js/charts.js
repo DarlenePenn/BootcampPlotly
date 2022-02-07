8da289e5b9b04622ea5601bff001ca7a50f7f349
DarlenePenn <93684851+DarlenePenn@users.noreply.github.com>
2022-02-07 00:15:44 -0500

    update charts file

--- js/charts.js
+++ js/charts.js
@@ -77,7 +77,7 @@ function buildCharts(sample) {
     //  so the otu_ids with the most bacteria are last. 
     var yticks = otuIds.map(sampleObj => "OTU " + sampleObj).slice(0,10).reverse();
 
-    console.log(yticks);
+    
   
     // 8. Create the trace for the bar chart. 
     var barData = [{
@@ -131,8 +131,8 @@ function buildCharts(sample) {
      
 
     // 2. Create a variable that holds the first sample in the metadata array.
-    var gaugeResult = gaugeArray[0];
-
+      var gaugeResult = gaugeArray[0];
+      console.log(wfreqs)
 
     // 3. Create a variable that holds the washing frequency.
     var wfreqs = gaugeResult.wfreq;
@@ -159,8 +159,7 @@ function buildCharts(sample) {
     }];
     
     // 5. Create the layout for the gauge chart.
-    var gaugeLayout = {
-      automargin = true     
+    var gaugeLayout = { width: 600, height: 500, automargin = true     
     };
 
     // 6. Use Plotly to plot the gauge data and layout.
